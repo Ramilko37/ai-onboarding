2930578b2fec58489ec742fdad3b1faa244e22df
Ramil Galyamdin <galyamdin@gmail.com>
2026-06-22 15:33:29 +0300

    Add separate HR manager service route

---
+++ app/hr/page.tsx
@@ -0,0 +1,5 @@
+import { MvpOnboardingPage } from "@/modules/onboarding-agent/ui/MvpOnboardingPage";
+
+export default function HrOnboardingPage() {
+  return <MvpOnboardingPage initialView="manager" />;
+}
